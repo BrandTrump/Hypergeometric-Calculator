508ff431e77eef872d3db8dbb163bd02a2adb04b
Brandon Trump <brandontrump101@gmail.com>
2023-03-13 17:11:20 +1300

    fetch card names from .ydk file

--- client/src/components/form/InputForm.jsx
+++ client/src/components/form/InputForm.jsx
@@ -1,23 +1,89 @@
 import styles from "../../styles/form/InputForm.module.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const InputForm = () => {
-  const [targetCards, setTargetCards] = useState([]);
-  const [targetCounts, setTargetCounts] = useState([]);
-  const [handSize, setHandSize] = useState(0);
-  const [drawSize, setDrawSize] = useState(0);
+  // const [targetCards, setTargetCards] = useState([]);
+  // const [targetCounts, setTargetCounts] = useState([]);
+  // const [handSize, setHandSize] = useState(0);
+  // const [drawSize, setDrawSize] = useState(0);
 
   const handleSubmit = (e) => {
     e.preventDefault();
   };
 
+  function fetchCardData(cardIds) {
+    const promises = cardIds.map((cardId) =>
+      fetch(`https://db.ygoprodeck.com/api/v7/cardinfo.php?id=${cardId}`)
+        .then((response) => response.json())
+        .then((card) => ({
+          id: cardId,
+          name: card.data[0].name,
+        }))
+    );
+    return Promise.all(promises);
+  }
+
+  function handleFileRead(event) {
+    const content = event.target.result;
+    const mainDeck = parseYDK(content);
+    fetchCardData(mainDeck)
+      .then((cardData) => {
+        console.log(cardData); // Display the card data
+      })
+      .catch((error) => {
+        console.error(error); // Log any errors
+      });
+  }
+
+  function handleFileInputChange(event) {
+    const file = event.target.files[0];
+    const reader = new FileReader();
+    reader.onload = handleFileRead;
+    reader.readAsText(file);
+  }
+
+  function handleFileRead(event) {
+    const content = event.target.result;
+    const deckList = parseYDK(content);
+    fetchCardData(deckList)
+      .then((cardData) => {
+        console.log(cardData);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  function parseYDK(content) {
+    const lines = content.split("\n");
+    const mainDeck = [];
+    let isExtraDeck = false;
+    let isSideDeck = false;
+    for (let i = 0; i < lines.length; i++) {
+      const line = lines[i].trim();
+      if (line === "#extra") {
+        isExtraDeck = true;
+        isSideDeck = false;
+      } else if (line === "!side") {
+        isExtraDeck = false;
+        isSideDeck = true;
+      } else if (line.length > 0 && !line.startsWith("#")) {
+        const cardId = parseInt(line);
+        if (!isNaN(cardId) && !isExtraDeck && !isSideDeck) {
+          mainDeck.push(cardId);
+        }
+      }
+    }
+    return mainDeck;
+  }
+
   return (
     <div className={styles.form_container}>
       <form className={styles.input_form} onSubmit={handleSubmit}>
         <label>Deck File:</label>
-        <input type="file" id="filename" accept=".txt" />
+        <input type="file" accept=".ydk" onChange={handleFileInputChange} />
 
-        <label>Target Cards:</label>
+        {/* <label>Target Cards:</label>
         <input
           type="text"
           id="targetCards"
@@ -53,7 +119,7 @@ const InputForm = () => {
           required
           onChange={(e) => setDrawSize(e.target.value)}
           value={drawSize}
-        />
+        /> */}
 
         <button id="calculate">Calculate</button>
       </form>
