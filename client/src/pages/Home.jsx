05675b64fe054d962ca9d49e5ad13e0cd2540709
Brandon Trump <brandontrump101@gmail.com>
2023-03-16 13:04:41 +1300

    created context that can be used across different child components

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,13 +1,22 @@
 import DeckUpload from "../components/DeckUpload";
 import InputForm from "../components/form/InputForm";
 import styles from "../styles/Home.module.css";
+import { useState } from "react";
+import { deckContext } from "../context/DeckContext";
 
 const Home = () => {
+  const [deckSize, setDeckSize] = useState(0);
+  const [targetCount, setTargetCount] = useState(0);
+
   return (
     <div className={styles.form_container}>
       <div className={styles.content}>
-        <DeckUpload />
-        <InputForm />
+        <deckContext.Provider
+          value={{ deckSize, setDeckSize, targetCount, setTargetCount }}
+        >
+          <DeckUpload />
+          <InputForm />
+        </deckContext.Provider>
       </div>
     </div>
   );
